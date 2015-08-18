651505fc58fc11f1c5fc6c49bcbba22bcc51ebfd
taro03 <taro03@users.noreply.github.com>
2015-08-17 21:51:23 -0700

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -19,69 +19,53 @@ views because none of the queries in the API would be simplified by using a
 view - there are no subqueries or compilcated constraints with more than one
 WHERE clause.
 
-tournaments
-------
-Stores information about each tournament.
-
-name (varchar, primary key) - The name of the tournament.
-
-event (varchar) - What sort of event the tournament is for (swimming, chess,
-cup-stacking, etc).
 
 
-contestants
-------
-Stores information about contestants for each tournament.
-
-id (bigserial, primary key) - The ID number of the contestant. Since
-no contestant should be entered into the same tournament twice, it is the
-primary key.
-
-name (varchar) - The player's actual human name.
-
-tournament (bigint) - which tournament this contestant is entered in. This
-column uses tournaments.name as a foreign key.
-
-wins (bigint) - How many matches this contestant won.
-
-losses (bigint) - How many matches this contestant loss
 
 
-matches
-------
-Stores information about matches and their outcomes.
-
-id (bigserial, primary key) - A unique identifier for the match.
-
-tournament (varchar) - which tournament this match was in. This
-column uses tournaments.name as a foreign key.
-
-winner (bigint) - The name of the first contestant; a foreign key for
-this column is contestants.id
-
-loser (bigint) - The name of the second contestant; a foreign key for
-this column is contestants.id
-
-*/
 DROP DATABASE IF EXISTS tournament;
 
 CREATE DATABASE tournament;
 
 \connect tournament;
 
+/*
+table players
+Stores information about players
+
+playerID (bigserial, primary key) - The ID number of the players. Since
+	no contestant should be entered into the same tournament twice, it is the
+	primary key.
+playerName (varchar) - The player's actual human name.
+*/
 create table players (
-    playerID BIGSERIAL PRIMARY KEY,
+    playerID BIGSERIAL PRIMARY KEY NOT NULL,
     playerName VARCHAR NOT NULL,
-    
 );
 
+/*
+table matches
+Stores information about matches and their outcomes.
+
+matchID (bigserial, primary key) - A unique identifier for the match.
+winner (integer) - The name of the first player; a foreign key for
+	this column is players.playerID
+loser (intefer) - The name of the second player; a foreign key for
+	this column is players.playername
+*/
 create table matches (
     matchID BIGSERIAL PRIMARY KEY,
     winner INTEGER REFERENCES players(playerID) NOT NULL,
     loser INTEGER REFERENCES players(playerID) NOT NULL
 );
 
-/*create view standings as
+/*
+view standings
+view standing will combine the table players and matches to filter out
+the player order by the most winning. The next match and the final result
+will based on the outcome of this view
+*/
+create view standings as
 	SELECT players.playerID, players.playerName,
 	(SELECT count(matches.winner)
 		FROM matches
@@ -92,5 +76,5 @@ create table matches (
 		WHERE players.playerID=matches.loser)
 	AS num_matches
 	FROM players
-	ORDER BY num_win DESC, num_matches DESC;*/
+	ORDER BY num_win DESC;
 
